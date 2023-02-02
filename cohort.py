a53bfa7c7d6818727db066ffecae9b1c3e3f109b
Fahad Ali <fahadali@live.ca>
2023-02-02 15:18:35 -0700

    Initialize files

---
+++ cohort.py
@@ -0,0 +1,11 @@
+'''
+Author: Fahad Ali
+Student ID: 3099218
+Description: This program will create cohorts of the best size depending on 
+the class space available, and suggest (if required) more class space
+'''
+
+class cohort:
+    
+    def __init__(self):
+        self._students = 0
